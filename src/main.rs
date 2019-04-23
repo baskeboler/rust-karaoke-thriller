959a0c2e485fc9851cc7a6400ec4e06d79100b53
Victor Gil <baskeboler@gmail.com>
2019-04-23 02:57:26 -0300

    added sprites and animations

--- src/main.rs
+++ src/main.rs
@@ -5,27 +5,35 @@ extern crate glutin_window;
 extern crate graphics;
 extern crate opengl_graphics;
 extern crate piston;
+// extern crate piston_window;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 extern crate chrono;
+extern crate rodio;
+#[macro_use]
 extern crate serde_json;
+extern crate ai_behavior;
+extern crate sprite;
+
 mod lyrics;
 // use graphics::character::CharacterCache;
-use glutin_window::GlutinWindow as Window;
-use opengl_graphics::*;
 // use graphics::*;
 // use opengl_graphics::graphics::character::CharacterCache;
 // use opengl_graphics::{GlGraphics, OpenGL, GlyphCache};
 
 // use graphics::glyph_cache::rusttype::GlyphCache;
+use ai_behavior::*;
+use chrono::{DateTime, Duration, Local};
 use colors::{Color, HslaColorType, RgbaColorType};
+use glutin_window::GlutinWindow as Window;
 use lyrics::{LyricProgressEvent, LyricsDisplay, LyricsFrame};
+use opengl_graphics::*;
 use piston::event_loop::*;
 use piston::input::*;
 use piston::window::WindowSettings;
 use serde::de::Deserialize;
-
+use sprite::*;
 struct App<G> {
     gl: G,
     rotation: f64,
@@ -35,14 +43,36 @@ struct App<G> {
     lyrics: LyricsDisplay,
     frames: Vec<LyricsFrame>,
     currentFrame: i8,
-
-    start: chrono::DateTime<chrono::Local>,
+    audio: Option<rodio::Sink>,
+    start: Option<chrono::DateTime<chrono::Local>>,
+    scene: sprite::Scene<opengl_graphics::Texture>,
 }
-
 impl App<GlGraphics> {
     fn calc_fps(&mut self, dt: f64) {
         self.fps = 1.0 / dt; //((self.fps * 4.0) + (1.0 / dt)) / 5.0;
     }
+    fn play(&mut self) {
+        self.start = chrono::Local::now()
+            .checked_sub_signed(chrono::Duration::milliseconds(1500))
+            .unwrap()
+            .into();
+
+        let device = rodio::default_output_device().unwrap();
+        let sink = rodio::Sink::new(&device);
+        let file = std::fs::File::open("thriller.mp3").unwrap();
+        sink.append(
+            rodio::Decoder::new(std::io::BufReader::with_capacity(10 * 1024 * 1024, file)).unwrap(),
+        );
+        self.audio = Some(sink);
+    }
+    fn stop(&mut self) {
+        self.start = None;
+        if let Some(a) = &self.audio {
+            a.stop();
+        }
+        self.audio = None;
+    }
+
     fn render_lyrics<C>(&mut self, args: RenderArgs, cache: &mut C)
     where
         C: graphics::character::CharacterCache<Texture = opengl_graphics::Texture, Error = String>,
@@ -53,15 +83,17 @@ impl App<GlGraphics> {
         let red: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
         let w1 = cache.width(24, fst).expect("width of processed text");
         let w2 = cache.width(24, snd).expect("width of unprocessed text");
-        println!("rendering text (w1={:?}, w2={:?})", w1, w2);
-
+        // println!("rendering text (w1={:?}, w2={:?})", w1, w2);
+        let l: u32 = (w1 + w2) as u32;
+        let x_offset = (args.width - l as f64) / 2.0;
         self.gl.draw(args.viewport(), |c, gl| {
-            let transform = c.transform.trans(20.0, 200.0);
+            let transform = c.transform.trans(x_offset, 200.0);
+            text(black, 24, fst, cache, transform.scale(1.0, 1.2), gl).unwrap();
             text(red, 24, fst, cache, transform, gl).unwrap();
             text(black, 24, snd, cache, transform.trans(w1, 0.0), gl).unwrap();
         });
     }
-    fn render<C>(&mut self, args: &RenderArgs, cache: &mut C)
+    fn render<C>(&mut self, e: &piston::input::Event, args: &RenderArgs, cache: &mut C)
     where
         C: graphics::character::CharacterCache<Texture = opengl_graphics::Texture, Error = String>,
     {
@@ -76,11 +108,13 @@ impl App<GlGraphics> {
         let (x, y) = (args.width / 2.0, args.height / 2.0);
         // let ref mut cache = self.glyph_cache;
         self.frame_count += 1;
-        self.calc_fps(args.ext_dt);
-        if self.frame_count % 50 == 0 {
-            println!("fps; {:?}", self.fps);
+        if self.frame_count % 60 * 60 == 0 {
+            // println!("fps; {:?}", self.fps);
+            println!("dt: {:?}", args.ext_dt);
         }
         let fps = self.fps;
+        // self.scene.event(e);
+        let scene = &self.scene;
         self.gl.draw(args.viewport(), |c, gl| {
             clear(primary_color, gl);
             let transform = c
@@ -90,7 +124,6 @@ impl App<GlGraphics> {
                 .trans(-0.5 * SQUARE_EDGE_SIZE, -0.5 * SQUARE_EDGE_SIZE);
             rectangle(secondary_color, square, transform, gl);
             // character::CharacterCache
-
             text(
                 secondary_color,
                 24,
@@ -109,17 +142,31 @@ impl App<GlGraphics> {
                 gl,
             )
             .unwrap();
+            scene.draw(c.transform.trans(x, y), gl);
         });
+        // {
+        //     self.gl.draw(args.viewport(), |c, gl| {
+        //         self.scene.draw(c.transform, gl);
+        //     });
+        // }
         self.render_lyrics(*args, cache);
     }
 
     fn update(&mut self, args: &UpdateArgs) {
         use chrono::Duration;
         self.rotation += 2.0 * args.dt;
-        let offset: Duration = chrono::Local::now() - self.start;
+        let offset: Duration = match self.start {
+            Some(s) => chrono::Local::now() - s,
+            None => Duration::zero(),
+        };
         let millis: f64 = offset.num_milliseconds() as f64;
 
-        let active: Vec<&LyricsFrame> = self.frames.iter().filter(|f| f.offset < millis).collect();
+        self.calc_fps(args.dt);
+        let active: Vec<&LyricsFrame> = self
+            .frames
+            .iter()
+            .filter(|f| f.offset < millis && !f.text.is_empty())
+            .collect();
         let t = active
             .last()
             .map(|l| l.text.clone())
@@ -137,7 +184,9 @@ impl App<GlGraphics> {
             .or(Some(0))
             .unwrap();
         self.lyrics.text = t;
-        self.lyrics.sing_progress.store(progress as usize, std::sync::atomic::Ordering::SeqCst);
+        self.lyrics
+            .sing_progress
+            .store(progress as usize, std::sync::atomic::Ordering::SeqCst);
     }
     fn key(&mut self, e: &Button) {
         match e {
@@ -167,22 +216,82 @@ impl App<GlGraphics> {
             }
             Button::Keyboard(Key::Space) => {
                 println!("advance lyrics!");
-                self.lyrics.advance();
+                self.stop();
+                self.play();
+            }
+            Button::Keyboard(Key::S) => {
+                use std::ops::Add;
+                self.stop();
+            }
+            Button::Keyboard(Key::A) => {
+                if let Some(s) = &self.start {
+                    self.start = s.checked_sub_signed(Duration::seconds(1)).into();
+                }
+            }
+            Button::Keyboard(Key::D) => {
+                if let Some(s) = &self.start {
+                    self.start = s.checked_add_signed(Duration::seconds(1)).into();
+                }
+                // if let Some(a) = &self.audio {
+                //     a.queue_tx.ptr.
+                // }
             }
             _ => println!("pressed {:?}", e),
         }
     }
 }
 
+fn get_animation() -> Behavior<Animation> {
+    let seq = Sequence(vec![
+        Action(Ease(
+            EaseFunction::CubicOut,
+            Box::new(ScaleTo(2.0, 0.5, 0.5)),
+        )),
+        Action(Ease(
+            EaseFunction::BounceOut,
+            Box::new(MoveBy(1.0, 0.0, 100.0)),
+        )),
+        Action(Ease(
+            EaseFunction::ElasticOut,
+            Box::new(MoveBy(2.0, 0.0, -100.0)),
+        )),
+        Action(Ease(
+            EaseFunction::BackInOut,
+            Box::new(MoveBy(1.0, 0.0, -100.0)),
+        )),
+        Wait(0.5),
+        Action(Ease(
+            EaseFunction::ExponentialInOut,
+            Box::new(MoveBy(1.0, 0.0, 100.0)),
+        )),
+        Action(Blink(1.0, 5)),
+        While(
+            Box::new(Wait(6.0)),
+            vec![
+                Action(Ease(EaseFunction::QuadraticIn, Box::new(FadeOut(1.0)))),
+                Action(Ease(EaseFunction::QuadraticOut, Box::new(FadeIn(1.0)))),
+            ],
+        ),
+    ]);
+    While(
+        Box::new(WaitForever),
+        vec![
+            seq,
+            Action(Ease(
+                EaseFunction::CubicOut,
+                Box::new(ScaleTo(5.0, 1.0, 1.0)),
+            )),
+        ],
+    )
+}
 fn main() {
     println!("Hello, world!");
-
     let opengl = OpenGL::V4_5;
-    let lyrics: Vec<LyricsFrame> =
-        std::fs::read_to_string("/home/victor/dev/clj-karaoke/song.json")
-            .map(|s| serde_json::de::from_str(s.as_str()).unwrap())
-            .unwrap();
-    println!("{:?}", lyrics);
+    let lyrics: Vec<LyricsFrame> = std::fs::read_to_string("song.json")
+        .map(|s| serde_json::de::from_str(s.as_str()).unwrap())
+        .unwrap();
+    println!("lyrics loaded");
+
     let mut window: Window = WindowSettings::new("spinning square", [400, 400])
         .opengl(opengl)
         .exit_on_esc(true)
@@ -192,6 +301,24 @@ fn main() {
     let assets = find_folder::Search::ParentsThenKids(3, 3)
         .for_folder("assets")
         .unwrap();
+
+    let mut scene = sprite::Scene::new();
+    let tex =
+        std::rc::Rc::new(Texture::from_path("infuy.svg.png", &TextureSettings::new()).unwrap());
+    let mut sprite = sprite::Sprite::from_texture(tex.clone());
+    sprite.set_position(0.0, 0.0);
+    sprite.set_anchor(0.5, 0.5);
+    let id = scene.add_child(sprite);
+
+    scene.run(id, &get_animation());
+    // This animation and the one above can run in parallel.
+    let rotate = Action(Ease(
+        EaseFunction::ExponentialInOut,
+        Box::new(RotateTo(2.0, 360.0)),
+    ));
+    scene.run(id, &rotate);
+
+    // scene.ru
     let font = assets.join("Bangers-Regular.ttf");
     // let factory = window.window.factory.clone();
     let mut app = App {
@@ -202,17 +329,24 @@ fn main() {
         color: RgbaColorType::new(0.2, 0.45, 0.3),
         lyrics: LyricsDisplay::new("hola como te va? uno dos tres cuatro cinco seis siete"),
         frames: lyrics,
-        start: chrono::Local::now(),
+        start: chrono::Local::now()
+            .checked_sub_signed(chrono::Duration::seconds(2))
+            .unwrap()
+            .into(),
         currentFrame: -1,
+        audio: None,
+        scene: scene,
     };
+    app.play();
     let mut glyph_cache = GlyphCache::new(font, (), TextureSettings::new()).unwrap();
 
     // let mut cache =;
-    let settings = EventSettings::new().max_fps(60);
-    let mut events = Events::new(settings);
+    let settings = EventSettings::new().max_fps(20);
+    let mut events = Events::new(settings).max_fps(30);
     while let Some(e) = events.next(&mut window) {
+        app.scene.event(&e);
         if let Some(r) = e.render_args() {
-            app.render(&r, &mut glyph_cache);
+            app.render(&e, &r, &mut glyph_cache);
         }
 
         if let Some(u) = e.update_args() {
